d7653d409e3aa64b30187908f5c468a3530e9fcb
206620219 <Brian.DeMaio@universalorlando.com>
2020-01-10 14:33:44 -0500

    Fill text and stroke text

--- script.js
+++ script.js
@@ -17,3 +17,13 @@ ctx.strokeRect(100, 200, 150, 100);
 //clearRect()
 //Clear out portion of rectangle on canvas
 ctx.clearRect(25, 25, 140, 90);
+
+//fillText()
+ctx.font = "30px Arial";
+ctx.fillStyle = "purple";
+ctx.fillText("Canvas is cool!", 400, 50);
+
+//strokeText()
+ctx.lineWidth = 1;
+ctx.strokeStyle = "orange";
+ctx.strokeText("Hello World!", 400, 100);
